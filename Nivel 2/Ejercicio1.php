e136ffd0604538ce6e0447ea117fe8c07222fdad
francobrida <francobrida@gmail.com>
2025-12-10 19:24:36 +0100

    Optimized Nivel2 Ejercicio1 without innecesary functions

--- Nivel 2/Ejercicio1.php
+++ Nivel 2/Ejercicio1.php
@@ -3,27 +3,15 @@
 $guestList1 = ["Alice", "Bob", "Charlie", "Mark", "Ethan"];
 $guestList2 = ["Diana", "Ethan", "Fiona", "Bob", "Peter"];
 
-$commonGuests = getCommonGuests($guestList1, $guestList2);
-$exclusiveGuestsList1 = getExclusiveGuests($guestList1, $commonGuests);
-$exclusiveGuestsList2 = getExclusiveGuests($guestList2, $commonGuests);
-
-function getCommonGuests(array $list1, array $list2): array {
-
-    $guestListInCommon = array_intersect($list1, $list2);
-    return $guestListInCommon;
-}
+$commonGuests = array_intersect($guestList1, $guestList2);
+$exclusiveGuestsList1 = array_diff($guestList1, $commonGuests);
+$exclusiveGuestsList2 = array_diff($guestList2, $commonGuests);
 
 function getAllGuests(array $list1, array $list2): array {
     $fullGuestList = array_merge($list1, $list2);
     $withoutDuplicates = array_unique($fullGuestList);
 
     return $withoutDuplicates;
-}
-
-function getExclusiveGuests(array $list, array $commonGuests): array {
-
-    $guestExclusives = array_diff($list, $commonGuests);
-    return $guestExclusives;
 }
 
 ?>
